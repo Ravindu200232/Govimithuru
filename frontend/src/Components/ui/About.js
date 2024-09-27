a4a303d65a06011bb1a59ee16ef73b16016b10f9
PATHIRAJA H P M H D S <147728182+hiru-D2@users.noreply.github.com>
2024-09-27 13:16:16 +0530

    ok

--- frontend/src/Components/ui/About.js
+++ frontend/src/Components/ui/About.js
@@ -18,21 +18,24 @@ const About = () => {
             <section className="content-section">
                 <div className="about-content">
                     <h2>Get to Know Us</h2>
-                    <h1>The Best Agriculture Market</h1>
-                    <p>
-                        There are many variations of passages of lorem ipsum available, but the majority have suffered alteration. 
-                        There are many variations of passages of lorem ipsum available but the majority have suffered alteration in some form.
+                    <h1>WHO WE ARE</h1>
+                    <p>ගොවි මිතුරු brings together a diverse team of experienced individuals who have worked across the agricultural sector in Sri Lanka and beyond. With backgrounds in both academic and practical study, we can assist with the design, build and ongoing supply for for all kinds of agricultural and horticultural projects across Sri Lanka.
+                        <br></br>
+                        We are proud to work in close collaboration with some of the leading greenhouse and irrigation system manufacturers in the world. Our high-quality grow tunnels, broad-spectrum irrigation systems other specialist agricultural products originate from manufacturers in Israel, Europe, Thailand, China, India and beyond.
+                        <br></br>
+                        Projects we have worked on include building some of the country’s largest export-orientated farms, island-wide CSR cluster farms for Sri Lanka’s largest corporations, farm-to-fork restaurants for high end resorts in Sri Lanka and the Maldives and the introduction of game-changing irrigation and fertigation systems across the country.
+                        <br></br>
+                        Alongside introducing Dutch seeds and technologies, ගොවි මිතුරු have also established a R&D centre in the up-country region of Sri Lanka where new varieties and methods are tested to address longstanding challenges faced by farmers and growers.
+                        <br></br>
+                        ගොවි මිතුරු understand the imprtance of knowledge-sharing across the industry and are proud to conduct and participate in frequent farmer outreach and training programmes throughout the country. We also frequently host international crops specialists and our farm facilities in Kadawatha and the hill country are open to visitors
                     </p>
-                    <ul>
-                        <li>Suscipit suscipit sagittis leo</li>
-                        <li>Rutrum estibulum dignissim posuere</li>
-                        <li>Lorem ipsum on the tend to repeat</li>
-                    </ul>
+                    
                     <button className="discover-btn">Discover More</button>
                 </div>
                 <div className="about-image">
                 <img src={img1} a alt="Agriculture Market" />
                 </div>
+                
             </section>
 
             {/* Team Members Section */}
